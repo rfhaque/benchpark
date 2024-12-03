46637496b5bd3ae0341f91e75bb31e1e2e9fb4f8
Dewi <43684384+dyokelson@users.noreply.github.com>
2024-12-03 08:04:31 -0800

    Qws experiment class (#387)
    * qws: rewrite as experiment object
    
    * qws: default to mpi instead of openmp
    
    * qws: fix style
    
    * Default openmp (that's the only version that exists)
    
    * Adding dry run
    
    * a few fixes, but not working yet, still a compiler error on dane
    
    * qws: rewrite as experiment object
    
    * qws: default to mpi instead of openmp
    
    * qws: fix style
    
    * Default openmp (that's the only version that exists)
    
    * Adding dry run
    
    * a few fixes, but not working yet, still a compiler error on dane
    
    * qws: rewrite as experiment object
    
    * qws: default to mpi instead of openmp
    
    * qws: fix style
    
    * Default openmp (that's the only version that exists)
    
    * Adding dry run
    
    * a few fixes, but not working yet, still a compiler error on dane
    
    * qws: rewrite as experiment object
    
    * qws: default to mpi instead of openmp
    
    * qws: fix style
    
    * Adding dry run
    
    * a few fixes, but not working yet, still a compiler error on dane
    
    * fixing weird tabbing issue
    
    * Adding dot slash
    
    * missing pipe in dry run
    
    * lint
    
    * format
    
    * Update experiment.py
    
    * move qws experiment
    
    * fix ci file
    
    * one dry run
    
    * update to inheritance model
    
    * fix spec
    
    * add license
    
    * add workload variant
    
    * edit dryrun
    
    * qws should work now
    
    * Update run.yml
    
    * remove experiments dir
    
    * black
    
    * fix dryrun
    
    * fix dryrun
    
    ---------
    
    Co-authored-by: Alec Scott <scott112@llnl.gov>
    Co-authored-by: pearce8 <pearce8@llnl.gov>

---
+++ experiments/qws/experiment.py
@@ -0,0 +1,61 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from benchpark.directives import variant
+from benchpark.experiment import Experiment
+from benchpark.openmp import OpenMPExperiment
+
+
+class Qws(Experiment, OpenMPExperiment):
+
+    variant(
+        "workload",
+        default="qws",
+        description="qws",
+    )
+
+    variant(
+        "version",
+        default="master",
+        description="app version",
+    )
+
+    def compute_applications_section(self):
+
+        self.add_experiment_variable("experiment_setup", "")
+        self.add_experiment_variable("lx", "32")
+        self.add_experiment_variable("ly", "6")
+        self.add_experiment_variable("lz", "4")
+        self.add_experiment_variable("lt", "3")
+        self.add_experiment_variable("px", "1")
+        self.add_experiment_variable("py", "1")
+        self.add_experiment_variable("pz", "1")
+        self.add_experiment_variable("pt", "1")
+        self.add_experiment_variable("tol_outer", "-1")
+        self.add_experiment_variable("tol_inner", "-1")
+        self.add_experiment_variable("maxiter_plus1_outer", "6")
+        self.add_experiment_variable("maxiter_inner", "50")
+
+        if self.spec.satisfies("+openmp"):
+            self.add_experiment_variable("n_nodes", ["1"], True)
+            self.add_experiment_variable("processes_per_node", ["1"])
+            self.add_experiment_variable("n_ranks", "{processes_per_node} * {n_nodes}")
+            self.add_experiment_variable("omp_num_threads", ["48"])
+            self.add_experiment_variable("arch", "OpenMP")
+
+    def compute_spack_section(self):
+        # get package version
+        app_version = self.spec.variants["version"][0]
+
+        system_specs = {}
+        system_specs["compiler"] = "default-compiler"
+        system_specs["mpi"] = "default-mpi"
+
+        # if package_spec left empty spack will use external
+        self.add_spack_spec(system_specs["mpi"])
+
+        self.add_spack_spec(
+            self.name, [f"qws@{app_version} +mpi", system_specs["compiler"]]
+        )
