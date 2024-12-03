591ad7b754d950eaa1792b0a5bd1106a4d99bc55
Peter Scheibel <scheibel1@llnl.gov>
2024-12-03 08:08:32 -0800

    Aws system: integrate 6a/7a configs (#468)
    * pull in static 7a/6a configs
    
    * class name fix; generate software.yaml properly; reference error to resource dir for packages.yaml; define extra mpirun-like opts
    
    * add dry run
    
    * quote error; consolidate into one line

--- systems/aws-pcluster/system.py
+++ systems/aws-pcluster/system.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import pathlib
+
 from benchpark.system import System
 from benchpark.directives import variant
 
@@ -17,21 +19,76 @@ id_to_resources = {
         "sys_cores_per_node": 4,
         "sys_mem_per_node": 8,
     },
+    "hpc7a.48xlarge": {
+        "sys_cores_per_node": 96,
+        "sys_mem_per_node": 768,
+    },
+    "hpc6a.48xlarge": {
+        "sys_cores_per_node": 96,
+        "sys_mem_per_node": 384,
+    },
 }
 
 
-class Aws(System):
+class AwsPcluster(System):
     variant(
         "instance_type",
-        values=("c6g.xlarge", "c4.xlarge"),
+        values=("c6g.xlarge", "c4.xlarge", "hpc7a.48xlarge", "hpc6a.48xlarge"),
         default="c4.xlarge",
         description="AWS instance type",
     )
 
     def initialize(self):
         super().initialize()
-        self.scheduler = "mpi"
+        self.scheduler = "slurm"
         # TODO: for some reason I have to index to get value, even if multi=False
         attrs = id_to_resources.get(self.spec.variants["instance_type"][0])
         for k, v in attrs.items():
             setattr(self, k, v)
+
+    def system_specific_variables(self):
+        return {
+            "extra_cmd_opts": '--mpi=pmix --export=ALL,FI_EFA_USE_DEVICE_RDMA=1,FI_PROVIDER="efa",OMPI_MCA_mtl_base_verbose=100',
+        }
+
+    def external_pkg_configs(self):
+        externals = AwsPcluster.resource_location / "externals"
+
+        selections = [
+            externals / "base" / "00-packages.yaml",
+        ]
+
+        return selections
+
+    def compiler_configs(self):
+        compilers = AwsPcluster.resource_location / "compilers"
+
+        selections = [
+            compilers / "gcc" / "00-gcc-7-compilers.yaml",
+        ]
+
+        return selections
+
+    def generate_description(self, output_dir):
+        super().generate_description(output_dir)
+
+        sw_description = pathlib.Path(output_dir) / "software.yaml"
+
+        with open(sw_description, "w") as f:
+            f.write(self.sw_description())
+
+    def sw_description(self):
+        return """\
+software:
+  packages:
+    default-compiler:
+      pkg_spec: gcc@7.3.1
+    default-mpi:
+      pkg_spec: openmpi@4.1.5%gcc@7.3.1
+    compiler-gcc:
+      pkg_spec: gcc@7.3.1
+    lapack:
+      pkg_spec: lapack@3.4.2
+    mpi-gcc:
+      pkg_spec: openmpi@4.1.5%gcc@7.3.1
+"""
