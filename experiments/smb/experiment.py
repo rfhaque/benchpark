79bed5e3fc712b44d5a318447aa893aea9ca7cac
august-knox <112430443+august-knox@users.noreply.github.com>
2024-12-16 16:58:49 -0800

    Adding Sandia microbenchmarks experiment class (#431)
    * initial commit for progress on sandia microbenchmarks
    
    * package changes
    
    * experiment class and package fixes
    
    * new experiment class, rma version
    
    * lint
    
    * lint
    
    * more lint
    
    * Delete experiments/smb/mpi directory
    
    remove old format
    
    * lint
    
    * removing hidden tab
    
    * lint
    
    * adding space between lines
    
    * adding dryruns
    
    * Indentation
    
    * removing bad indent in dryrun
    
    ---------
    
    Co-authored-by: august-knox <knox10@llnl.gov>
    Co-authored-by: pearce8 <pearce8@llnl.gov>

---
+++ experiments/smb/experiment.py
@@ -0,0 +1,45 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from benchpark.directives import variant
+from benchpark.experiment import Experiment
+from benchpark.scaling import StrongScaling
+
+
+class Smb(Experiment, StrongScaling):
+    variant(
+        "workload",
+        default="mpi_overhead",
+        values=("mpi_overhead", "msgrate", "rma_mt"),
+        description="workload",
+    )
+
+    variant(
+        "version",
+        default="master",
+        description="app version",
+    )
+
+    def compute_applications_section(self):
+        if self.spec.satisfies("workload=mpi_overhead"):
+            self.add_experiment_variable("n_ranks", "2")
+        elif self.spec.satisfies("workload=msgrate") or self.spec.satisfies(
+            "workload=rma_mt"
+        ):
+            self.add_experiment_variable("n_nodes", "1")
+            self.add_experiment_variable("n_ranks", "{n_nodes}*{sys_cores_per_node}")
+
+    def compute_spack_section(self):
+        # get package version
+        app_version = self.spec.variants["version"][0]
+
+        spec_string = f"smb@{app_version} +mpi"
+        if self.spec.satisfies("workload=rma_mt"):
+            spec_string += "+rma"
+        system_specs = {}
+        system_specs["compiler"] = "default-compiler"
+        system_specs["mpi"] = "default-mpi"
+        self.add_spack_spec(system_specs["mpi"])
+        self.add_spack_spec(self.name, [spec_string, system_specs["compiler"]])
